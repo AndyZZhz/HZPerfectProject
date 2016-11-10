19850f119aeed55dcab9e6f135699b47d1a827ab
张好志 <zhihaozhang@anjuke.com>
2016-11-10 14:20:54 +0800

    Initial Commit

---
+++ HZPerfectProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HZPerfectProject
+//
+//  Created by Andyzzhz on 2016/11/10.
+//  Copyright © 2016年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HZPerfectProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HZPerfectProject
+//
+//  Created by Andyzzhz on 2016/11/10.
+//  Copyright © 2016年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
